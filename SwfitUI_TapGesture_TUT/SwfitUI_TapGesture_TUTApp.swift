a24b334c38ae0883660634ff2e5253be4ab8abef
SeonJungKim <ksj083895@gmail.com>
2021-03-24 14:33:35 +0500

    Initial Commit

---
+++ SwfitUI_TapGesture_TUT/SwfitUI_TapGesture_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUI_TapGesture_TUTApp.swift
+//  SwfitUI_TapGesture_TUT
+//
+//  Created by 김선중 on 2021/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUI_TapGesture_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
